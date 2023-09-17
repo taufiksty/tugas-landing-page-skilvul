f2ba0e136387aad089b024590ded2d5258b772b4
taufiksty <taufik.satya09@gmail.com>
2023-09-17 16:38:05 +0700

    add form and validation

--- script.js
+++ script.js
@@ -9,3 +9,50 @@ menuMobileOpen.addEventListener('click', () => {
 menuMobileClose.addEventListener('click', () => {
 	menuMobile.classList.toggle('menu-opened');
 });
+
+function handleGetFormData() {
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const city = document.getElementById('city').value;
+	const zipCode = document.getElementById('zip-code').value;
+	const status = document.getElementById('status').checked;
+
+	return { name, email, city, zipCode, status };
+}
+
+function isNumber(zipCode) {
+	const number = parseInt(zipCode);
+
+	return !isNaN(number);
+}
+
+function checkboxIsChecked(status) {
+	return status;
+}
+
+function validateFormData(formData) {
+	if (
+		formData &&
+		isNumber(formData.zipCode) &&
+		checkboxIsChecked(formData.status)
+	) {
+		return true;
+	}
+
+	return false;
+}
+
+function submit() {
+	event.preventDefault();
+	const warning = document.getElementById('warning');
+
+	const formData = handleGetFormData();
+
+	if (!validateFormData(formData)) {
+		warning.innerHTML = 'Periksa form anda sekali lagi';
+	} else {
+		warning.innerHTML = '';
+	}
+}
+
+document.getElementById('form').addEventListener('submit', () => submit());
